b90a6d70b2713280a837251b78199f25ef1fb794
Margokomilfo-dev <margokomilfo@mail.ru>
2020-12-01 02:17:52 +0100

    My Resume

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { AboutMe } from './components/AboutMe/AboutMe';
 import { Header } from './components/Header/Header';
+import {MyResume} from "./components/MyResume/MyResume";
 
 function App() {
   return (
     <div className="App">
         <Header/>
         <AboutMe/>
+        <MyResume/>
     </div>
   );
 }
---
+++ src/components/MyResume/MyResume.tsx
@@ -0,0 +1,122 @@
+import React from "react"
+import s from './MyResume.module.css'
+
+type MyResumePropsType = {}
+
+export const MyResume: React.FC<MyResumePropsType> = (props) => {
+    return (
+        <div className={s.myResume}>
+            <div className={s.container}>
+                <div className={s.title}>
+                    <h2>My Resume</h2>
+                    <span className={s.border}> </span>
+                </div>
+                <div className={s.resume}>
+                    <div className={s.timeLine}>
+                        <div className={s.category}>
+                            <span>Experiences</span>
+                            <div className={s.categoryIcon}></div>
+                        </div>
+
+                        <div className={s.contentBlocks}>
+                            <div className={s.left}>
+                                <div className={s.blocks}>
+                                    <div className={`${s.block} ${s.blockLeft}`}>
+                                        <div className={`${s.blockIcon} ${s.iconLeft}`}></div>
+                                        <h4>position</h4>
+                                        <p>Place - date</p>
+                                        <p>Lorem ipsum dolor sit amet, corporis explicabo maiores minima numquam
+                                            possimus, praesentium
+                                            tempora veniam voluptas? Ab quos rem sunt. consectetur adipisicing elit.
+                                            Debitis, facilis?
+                                        </p>
+                                    </div>
+                                    <div className={`${s.block} ${s.blockLeft}`}>
+                                        <div className={`${s.blockIcon} ${s.iconLeft}`}></div>
+                                        <h4>position</h4>
+                                        <p>Place - date</p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, facilis?
+                                            corporis explicabo maiores minima numquam possimus, praesentium
+                                            tempora veniam voluptas? Ab quos rem sunt.</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className={s.right}>
+                                <div className={s.blocks}>
+                                    <div className={`${s.block} ${s.blockRight}`}>
+                                        <div className={`${s.blockIcon} ${s.iconRight}`}></div>
+                                        <h4>position</h4>
+                                        <p>Place - date</p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, facilis?
+                                            Ab quos rem sunt. consectetur adipisicing elit. Debitis, facilis</p>
+                                    </div>
+                                    <div className={`${s.block} ${s.blockRight}`}>
+                                        <div className={`${s.blockIcon} ${s.iconRight}`}></div>
+                                        <h4>position</h4>
+                                        <p>Place - date</p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis,
+                                            facilis?</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={s.category}>
+                            <span>Education</span>
+                            <div className={s.categoryIcon}></div>
+                        </div>
+
+
+                        <div className={s.contentBlocks}>
+                            <div className={s.left}>
+                                <div className={s.blocks}>
+                                    <div className={`${s.block} ${s.blockLeft}`}>
+                                        <div className={`${s.blockIcon} ${s.iconLeft}`}></div>
+                                        <h4>position</h4>
+                                        <p>Place - date</p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis,
+                                            facilis?</p>
+                                    </div>
+                                    <div className={`${s.block} ${s.blockLeft}`}>
+                                        <div className={`${s.blockIcon} ${s.iconLeft}`}></div>
+                                        <h4>position</h4>
+                                        <p>Place - date</p>
+                                        <p>Lorem ipsum dolor sit amet, corporis explicabo maiores minima numquam
+                                            possimus, praesentium
+                                            tempora veniam voluptas? Ab quos rem sunt. consectetur adipisicing elit.
+                                            Debitis, facilis?</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className={s.right}>
+                                <div className={s.blocks}>
+                                    <div className={`${s.block} ${s.blockRight}`}>
+                                        <div className={`${s.blockIcon} ${s.iconRight}`}></div>
+                                        <h4>position</h4>
+                                        <p>Place - date</p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis,
+                                            facilis?</p>
+                                    </div>
+                                    <div className={`${s.block} ${s.blockRight}`}>
+                                        <div className={`${s.blockIcon} ${s.iconRight}`}></div>
+                                        <h4>position</h4>
+                                        <p>Place - date</p>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, facilis?
+                                            Lorem ipsum dolor sit amet, consectetur
+                                            adipisicing elit. Aliquid ea esse officia quasi
+                                            velit? Aut commodi corporis explicabo maiores minima numquam possimus,
+                                            praesentium
+                                            tempora veniam voluptas? Ab quos rem sunt.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={s.categoryEndIcon}></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
