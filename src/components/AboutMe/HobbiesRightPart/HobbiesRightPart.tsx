bb8879b252113948953a611fe10a77182a23ccaa
Margokomilfo-dev <margokomilfo@mail.ru>
2020-12-26 02:05:15 +0100

    portfolio animation+ js

--- src/components/AboutMe/HobbiesRightPart/HobbiesRightPart.tsx
+++ src/components/AboutMe/HobbiesRightPart/HobbiesRightPart.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import s from './HobbiesRightPart.module.sass'
 import {v1} from 'uuid'
-import sport from '../../../assets/img/dumbbell.png'
-import book from '../../../assets/img/book.png'
-import family from '../../../assets/img/house.png'
-import paw from '../../../assets/img/paw.png'
-import plane from '../../../assets/img/airplane.png'
-import mac from '../../../assets/img/mac-os-logo.png'
-import money from '../../../assets/img/money.png'
-import cinema from '../../../assets/img/video.png'
-import sweet from '../../../assets/img/sweet.png'
-import coffee from '../../../assets/img/coffee.png'
+import sport from '../../../assets/img/icons/dumbbell.png'
+import book from '../../../assets/img/icons/book.png'
+import family from '../../../assets/img/icons/house.png'
+import paw from '../../../assets/img/icons/paw.png'
+import plane from '../../../assets/img/icons/airplane.png'
+import mac from '../../../assets/img/icons/mac-os-logo.png'
+import money from '../../../assets/img/icons/money.png'
+import cinema from '../../../assets/img/icons/video.png'
+import sweet from '../../../assets/img/icons/sweet.png'
+import coffee from '../../../assets/img/icons/coffee.png'
+import habr from '../../../assets/img/icons/info.png'
+import stackoverflow from '../../../assets/img/icons/overflowing.png'
+import codewars from '../../../assets/img/icons/html-coding.png'
 
 
 type StateType = { id: string,  name: string, img: any }
@@ -18,12 +21,15 @@ const state: Array<StateType> = [
     {id: v1(), name: 'Sport', img: sport },
     {id: v1(), name: 'Books', img: book },
     {id: v1(), name: 'Family', img: family },
+    {id: v1(), name: 'codewars', img: codewars },
     {id: v1(), name: 'Animals', img: paw },
     {id: v1(), name: 'Travels', img: plane },
     {id: v1(), name: 'Mac OS', img: mac },
     {id: v1(), name: 'Money', img: money },
     {id: v1(), name: 'Cinema', img: cinema },
-    {id: v1(), name: 'Sweets', img: sweet },
+    {id: v1(), name: 'stackoverflow', img: stackoverflow },
+    // {id: v1(), name: 'Sweets', img: sweet },
+    {id: v1(), name: 'habr', img: habr },
     {id: v1(), name: 'Coffee', img: coffee },
 ]
 
--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,14 +1,43 @@
-import React from "react"
-import s from './Portfolio.module.css'
+import React, {useState} from 'react'
+import s from './Portfolio.module.sass'
 import '../../common.css'
 import styles from '../../common/commonStyles.module.sass'
+import {v1} from 'uuid'
+import bg1 from '../../assets/img/1.jpg'
+import bg2 from '../../assets/img/2.jpg'
+import bg3 from '../../assets/img/3.jpg'
 
+type WorkType = { id: string, style: any, title: string, description: string, part:FilterType }
+type FilterType = 'all' | 'js' | 'html-css' | 'html-css-js' | 'react-redux' | 'small-tasks'
+
+
+const social = {backgroundImage: `url(${bg1})`}
+const todo = {backgroundImage: `url(${bg2})`}
+const project = {backgroundImage: `url(${bg3})`}
+
+const initialState = [
+        {id: v1(), style: social, title: 'Social Network JavaScript', description: 'developed for somebody', part: 'react-redux'},
+        {id: v1(), style: todo, title: 'Todolist', description: 'developed for somebody', part: 'react-redux'},
+        {id: v1(), style: project, title: 'Counter', description: 'developed for somebody', part: 'react-redux'},
+        {id: v1(), style: project, title: 'CRM', description: 'developed for somebody', part: 'html-css-js'},
+        {id: v1(), style: todo, title: 'Lending', description: 'developed for somebody', part: 'html-css-js'},
+    ] as Array<WorkType>
 
 type PortfolioPropsType = {}
 
 export const Portfolio: React.FC<PortfolioPropsType> = (props) => {
+    const [state, setState] = useState<Array<WorkType>>(initialState)
+    const sortWorks = (state: Array<WorkType>, filter: FilterType): Array<WorkType> => {
+        if (filter === 'all') return state.filter(w => w.part === 'js' || w.part === 'html-css-js' || w.part ===  'react-redux' || w.part === 'small-tasks')
+        if (filter === 'js') return state.filter(w => w.part === 'js' || w.part === 'html-css-js' )
+        if (filter === 'html-css') return state.filter(w => w.part === 'html-css')
+        if (filter === 'html-css-js') return state.filter(w => w.part === 'html-css-js')
+        if (filter === 'react-redux') return state.filter(w => w.part ===  'react-redux')
+        if (filter === 'small-tasks') return state.filter(w => w.part === 'small-tasks')
+        return state
+    }
     return (
-        <div className={styles.blockName}  data-aos="fade-in" id='works'>
+        <div className={styles.blockName} data-aos="fade-in" id='works'>
             <div className={styles.container}>
                 <div className={styles.blockTitle}>
                     <h2>Portfolio</h2>
@@ -16,83 +45,40 @@ export const Portfolio: React.FC<PortfolioPropsType> = (props) => {
                 </div>
                 <div className={s.menu}>
                     <ul>
-                        <li className={s.selected}>All works</li>
-                        <li>JS</li>
-                        <li>HTML/CSS</li>
-                        <li>HTML/CSS/JS</li>
-                        <li>React</li>
-                        <li>React/Redux</li>
-                        <li>Small tasks </li>
+                        <li onClick={() => setState(sortWorks(initialState, 'all'))}>All works</li>
+                        <li onClick={() => setState(sortWorks(initialState, 'js'))}>JavaScript</li>
+                        <li onClick={() => setState(sortWorks(initialState, 'html-css'))}>HTML/CSS</li>
+                        <li onClick={() => setState(sortWorks(initialState, 'html-css-js'))}> HTML/CSS/JS</li>
+                        <li onClick={() => setState(sortWorks(initialState, 'react-redux'))}>React & React/Redux</li>
+                        <li onClick={() => setState(sortWorks(initialState, 'small-tasks'))}>small tasks</li>
                     </ul>
                 </div>
 
                 <div className={s.portfolioContainer}>
-                    <div className={s.work}>
-                        <div className={s.details}>
-                            <h2>portfolio title design</h2>
-                            <p>developed for <span>somebody</span></p>
-                        </div>
-                    </div>
-
-                    <div className={s.work}>
-                        <div className={s.details}>
-                            <h2>portfolio title design</h2>
-                            <p>developed for <span>somebody</span></p>
-                        </div>
-                    </div>
-
-                    <div className={s.work}>
-                        <div className={s.details}>
-                            <h2>portfolio title design</h2>
-                            <p>developed for <span>somebody</span></p>
-                        </div>
-                    </div>
-
-
-                    <div className={s.work}>
-                        <div className={s.details}>
-                            <h2>portfolio title design</h2>
-                            <p>developed for <span>somebody</span></p>
-                        </div>
-                    </div>
-
-
-                    <div className={s.work}>
-                        <div className={s.details}>
-                            <h2>portfolio title design</h2>
-                            <p>developed for <span>somebody</span></p>
-                        </div>
-                    </div>
-
-                    <div className={s.work}>
-                        <div className={s.details}>
-                            <h2>portfolio title design</h2>
-                            <p>developed for <span>somebody</span></p>
-                        </div>
-                    </div>
-
-                    <div className={s.work}>
-                        <div className={s.details}>
-                            <h2>portfolio title design</h2>
-                            <p>developed for <span>somebody</span></p>
-                        </div>
-                    </div>
-
-                    <div className={s.work}>
-                        <div className={s.details}>
-                            <h2>portfolio title design</h2>
-                            <p>developed for <span>somebody</span></p>
-                        </div>
-                    </div>
-
-                    <div className={s.work}>
-                        <div className={s.details}>
-                            <h2>portfolio title design</h2>
-                            <p>developed for <span>somebody</span></p>
-                        </div>
-                    </div>
-
+                    {state.map(w => <WorkBlock style={w.style} title={w.title}
+                                               description={w.description} />)}
                 </div>
+            </div>
+        </div>
+    )
+}
+
+type WorkBlockType = {
+    title: string
+    description: string
+    style: any
+}
+export const WorkBlock: React.FC<WorkBlockType> = (props) => {
+
+    return (
+        <div className={s.work} style={props.style}>
+            <div className={s.curtain}>
+                <a href="">
+                    <div className={s.details}>
+                        <h2>{props.title}</h2>
+                        <p>{props.description}<span> somebody</span></p>
+                    </div>
+                </a>
             </div>
         </div>
     )
